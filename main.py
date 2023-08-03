db72c8a12ed95944a79f1cccd1520cff347c7eac
gary <kikz270@gmail.com>
2023-08-03 13:05:04 -0500

    data loading complete - sample visualizations shown - gpt optimizedC:/Python311/python.exe

--- main.py
+++ main.py
@@ -1,70 +1,103 @@
 import pandas as pd
 import psycopg2
+import plotly.express as px
+import configparser
+
+# Load the configuration from the 'config.ini' file
 
 
-def main():
-    # Load the CSV file
-    selector = 'SELECT * FROM public.peru_regional_data'
-    fetch_data_from_database(selector)
+def load_configuration():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config
 
-    # 3. Clean and preprocess the data
-
-    # Handle missing values (if any)
-
-    # Convert data types (if needed)
-    # data['date_column'] = pd.to_datetime(data['date_column'])
-
-    # Normalize or scale data (if needed)
-    # e.g., data['value_column'] = (data['value_column'] - data['value_column'].mean()) / data['value_column'].std()
-
-    # 4. Transform the data into the desired structure
-    # e.g., Group by region and calculate summary statistics
-    # grouped_data = data.groupby('region').agg({'value_column': ['mean', 'sum']})
-
-    # 5. Split the data based on regions or other criteria
-    # e.g., create a dictionary of DataFrames for each region
-    # region_data = {region: data[data['region'] == region] for region in data['region'].unique()}
-
-    # 6. Optimize data for performance (if dealing with large datasets)
-    # Consider using efficient data structures or caching methods
-
-    # 7. Validate the prepared data (optional)
-    # e.g., perform some basic plots or statistical analyses to ensure data is as expected
-
-    # 8. Create a data access layer (optional)
-    # Consider encapsulating data loading and preprocessing in separate functions or modules
-
-    # 9. Document the process (within code comments and/or external documentation)
+# Fetch data from the PostgreSQL database based on a given SQL query
 
 
-def fetch_data_from_database(selector):
-    # Try to connect to the database
+def fetch_data_from_database(selector, db_config):
     try:
         conn = psycopg2.connect(
-            host="pixel.ourcloud.ou.edu",
-            port=5432,
-            database="panviz",
-            user="panviz_readonly",
-            password="T3u&c7U58V9H"
+            host=db_config['host'],
+            port=db_config['port'],
+            database=db_config['database'],
+            user=db_config['user'],
+            password=db_config['password']
         )
+        cursor = conn.cursor()
+        cursor.execute(selector)
+        results = cursor.fetchall()
+        cursor.close()  # Close the cursor to release resources
+        conn.close()
+        return results
     except Exception as err:
-        # If the connection fails, print the error message and exit the program
         print("Unable to connect! Exiting! Error:", err)
         exit()
 
-    # If the connection is successful, fetch the data
-    cursor = conn.cursor()
-    cursor.execute(selector)
-    results = cursor.fetchall()
+# Load the cases and deaths data from the database
 
-    # Print out the first line of the returned data
-    print("First line of data:", results[0])
 
-    # Close the connection to the database
-    conn.close()
+def load_data(db_config):
+    selector_cases = 'SELECT "departmento" FROM public."PERUPositiveRawData"'
+    selector_deaths = 'SELECT "departamento" FROM public."PERUDeathRawData"'
+    data_cases = fetch_data_from_database(selector_cases, db_config)
+    data_deaths = fetch_data_from_database(selector_deaths, db_config)
+    return pd.DataFrame(data_cases, columns=['region']), pd.DataFrame(data_deaths, columns=['region'])
 
-    # Return the fetched data
-    return results
+# Preprocess the data by handling missing values and adding aggregation columns
+
+
+def preprocess_data(df_cases, df_deaths):
+    df_cases.dropna(inplace=True)
+    df_deaths.dropna(inplace=True)
+    df_cases['cases'] = 1
+    df_deaths['deaths'] = 1
+    return df_cases, df_deaths
+
+# Transform the data by grouping and merging cases and deaths data
+
+
+def transform_data(df_cases, df_deaths):
+    grouped_cases = df_cases.groupby('region').agg(
+        {'cases': 'sum'}).reset_index()
+    grouped_deaths = df_deaths.groupby('region').agg(
+        {'deaths': 'sum'}).reset_index()
+    return pd.merge(grouped_cases, grouped_deaths, on='region', suffixes=('_cases', '_deaths'))
+
+# Create a bar plot using Plotly Express with specified parameters
+
+
+def create_plot(df, x, y, title, plot_config):
+    return px.bar(df, x=x, y=y, title=title, height=plot_config['height'], width=plot_config['width'])
+
+# Main function to orchestrate the data loading, preprocessing, transformation, and visualization
+
+
+def main():
+    # Load configuration from file
+    config = load_configuration()
+    # Get database configuration
+    db_config = config['database']
+    # Get plot configuration
+    plot_config = {key: int(value) for key, value in config['plot'].items()}
+
+    # Load cases and deaths data
+    df_cases, df_deaths = load_data(db_config)
+    df_cases, df_deaths = preprocess_data(
+        df_cases, df_deaths)  # Preprocess the data
+    # Transform the data
+    merged_df = transform_data(df_cases, df_deaths)
+
+    # Create and show the plots
+    fig_cases = create_plot(df_cases, 'region', 'cases',
+                            'Confirmed Cases by Region', plot_config)
+    fig_deaths = create_plot(
+        df_deaths, 'region', 'deaths', 'Confirmed deaths by Region', plot_config)
+    fig_combined = create_plot(merged_df, 'region', [
+                               'cases', 'deaths'], 'Comparison of Cases and Deaths by Region', plot_config)
+
+    fig_cases.show()
+    fig_deaths.show()
+    fig_combined.show()
 
 
 if __name__ == "__main__":
